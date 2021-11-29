0dbf21e4c42b7e0fb94e92303157e4c80ee6d2be
beslimir <marin.beslo@gmail.com>
2021-11-29 10:12:25 +0100

    parse local json file; insert to DB; show in recyclerView

--- app/src/main/java/com/example/notifyme/feature_notifications/presentation/notifications/view_model/NotificationsViewModel.kt
+++ app/src/main/java/com/example/notifyme/feature_notifications/presentation/notifications/view_model/NotificationsViewModel.kt
@@ -2,7 +2,7 @@ package com.example.notifyme.feature_notifications.presentation.notifications.vi
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.notifyme.feature_notifications.domain.model.NotificationDetails
 import com.example.notifyme.feature_notifications.domain.model.NotificationItem
@@ -15,12 +15,20 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import java.io.InputStream
 import javax.inject.Inject
+import android.app.Application
+import com.google.gson.Gson
+import org.json.JSONObject
+
+import org.json.JSONArray
+
 
 @HiltViewModel
 class NotificationsViewModel @Inject constructor(
-    private val useCases: UseCasesWrapper
-): ViewModel() {
+    private val useCases: UseCasesWrapper,
+    application: Application
+) : AndroidViewModel(application) {
 
     private val _state = mutableStateOf(NotificationState())
     val state: State<NotificationState> = _state
@@ -29,6 +37,25 @@ class NotificationsViewModel @Inject constructor(
 
     init {
 //        insertTemporaryNotifications()
+
+
+        //get all data from local json file
+        val myJson = getJsonFromLocalFile(application.assets.open("notify_me_msg.json"))
+
+        //save retrieved data to Room db
+        saveRetrievedDataToRoomDb(myJson)
+
+
+//        val reader = JSONObject(myJson)
+//        val finalJson: JSONArray = reader.getJSONArray("final_json")
+//
+//        val list = arrayListOf<NotificationItem>()
+//        for (i in 0 until finalJson.length()) {
+//            list.add(Gson().fromJson(finalJson[i].toString(), NotificationItem::class.java))
+//        }
+
+
+        //show all notifications from Room database in app list
         getAllNotifications(OrderType.Ascending)
     }
 
@@ -70,14 +97,42 @@ class NotificationsViewModel @Inject constructor(
         }
     }
 
+    private fun getJsonFromLocalFile(inputStream: InputStream): String {
+        var json: String? = null
+
+        var bytes = ByteArray(inputStream.available())
+        inputStream.read(bytes, 0, bytes.size)
+        json = String(bytes)
+
+        return json
+    }
+
+    private fun saveRetrievedDataToRoomDb(myJson: String) {
+        val reader = JSONObject(myJson)
+        val finalJson: JSONArray = reader.getJSONArray("final_json")
+
+        viewModelScope.launch {
+            for (i in 0 until finalJson.length()) {
+                useCases.insertNotificationUseCase(
+                    Gson().fromJson(
+                        finalJson[i].toString(),
+                        NotificationItem::class.java
+                    )
+                )
+            }
+        }
+
+    }
+
     private fun getAllNotifications(orderType: OrderType) {
         getNotificationsJob?.cancel()
-        getNotificationsJob = useCases.getAllNotificationsUseCase(orderType).onEach { notifications ->
-            _state.value = state.value.copy(
-                notifications = notifications,
-                orderType = orderType
-            )
-        }.launchIn(viewModelScope)
+        getNotificationsJob =
+            useCases.getAllNotificationsUseCase(orderType).onEach { notifications ->
+                _state.value = state.value.copy(
+                    notifications = notifications,
+                    orderType = orderType
+                )
+            }.launchIn(viewModelScope)
     }
 
 }
