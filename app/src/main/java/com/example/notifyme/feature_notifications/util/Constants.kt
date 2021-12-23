3309fee8240850b8855ae99693a7a6160373c192
beslimir <marin.beslo@gmail.com>
2021-12-23 10:15:50 +0100

    fix bug with data type

--- app/src/main/java/com/example/notifyme/feature_notifications/util/Constants.kt
+++ app/src/main/java/com/example/notifyme/feature_notifications/util/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val START_DATE = "01.01.1900."
     const val END_DATE = "01.01.2100."
     const val TAG = "beslimir"
-    const val ONE_DAY_IN_MILLIS = 86400000
+    const val ONE_DAY_IN_MILLIS = 86400000L
 
     const val ICON_TYPE_HEART = "heart"
     const val ICON_TYPE_FIRE = "fire"
